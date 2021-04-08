4e1f9f648c0a71f9cec20769207d573ef4618d90
Ben Murray <murraybp64@gmail.com>
2021-04-08 16:05:33 -0600

    Start notebook, update dependencies

--- shell.nix
+++ shell.nix
@@ -10,9 +10,13 @@ mkShell {
     python37Packages.scikitlearn
     python37Packages.scipy
     python37Packages.matplotlib
+    python37Packages.pandas
 
     # Deep learning libraries
     python37Packages.pytorchWithCuda
+    python37Packages.opencv
+    python37Packages.transformers
+    python37Packages.torchvision
 
     # Notebook
     python37Packages.notebook
